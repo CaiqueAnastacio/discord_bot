4274d0e3cf01714989d41a8407e87b285e9ba5d1
Caíque Anastácio <51143057+CaiqueAnastacio@users.noreply.github.com>
2026-07-24 19:42:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,22 +25,27 @@ async def on_presence_update(before, after):
     print("Evento disparou!")
     # Ignora qualquer outro usuário
     if after.id != CHINA_ID:
+        print("not china!")
         return
 
     # Ignora se o status não mudou
     if before.status == after.status:
+        print("nao mudou status!")
         return
 
     canal = bot.get_channel(CANAL_ID)
 
     if canal is None:
+        print(f"o canal ta None")
         return
 
     if after.status == discord.Status.online:
+        print("FOI PRA ONELINE")
         await canal.send(f"{after.display_name} detectado!")
         # await canal.send(f"🟢 {after.display_name} ficou online!")
 
     elif after.status == discord.Status.offline:
+        print("FOI OFFLAINE")
         await canal.send(f"{after.display_name} sumiu!")
         # await canal.send(f"⚫ {after.display_name} ficou offline!")
 
